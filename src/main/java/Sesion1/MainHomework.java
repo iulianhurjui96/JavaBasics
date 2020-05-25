27fe779f1f23e5117e641042004056a4783a3fee
Adrian Hurjui <adrian.hurjui@endava.com>
2020-05-25 15:02:37 +0300

    Added homework

---
+++ src/main/java/Sesion1/MainHomework.java
@@ -0,0 +1,37 @@
+package Sesion1;
+
+public class MainHomework {
+    public static void main(String[] args) {
+
+        // HOMEWORK//
+            //Exercitiu 1
+            //todo - Creeaza un vector de String-uri;
+            //todo - Adauga in vector 6 prenume de persoane [Andrei, Alex, Ioana, Adrian, Liviu, Cosmin];
+            //todo - Itereaza vectorul si printeaza toate numele care contin litera "a" in prenume;
+            //Exercitiu 2
+            //todo - Creeaza un vector de double
+            //todo - Adauga in vector 10 integer-uri [10, 11, 13, 15, 18, 17, 20, 25, 31, 49]
+            //todo - Adauga in vector 4 double-uri [10.5, 16.8, 19.5, 20.00]
+            //todo - Itereaza vectorul si printeaza toate numerele >= 10 si <=20 care nu sunt de tip double
+            //Exercitiu 3
+            //todo - Avand urmatorul detaliu: Cerc cu un diametru de 7
+            //todo - Sa se afle aria discului de cerc. Dar nu prin formula (pi * raza la patrat). Valoarea lui pi = 3.14;
+            //todo - Printeaza raspunsul: Cercul indeplineste cerintele dvs, avand aria: + aria -> daca aria e mai mare ca 38
+            //todo - Printeaza raspunsul: Cercul nu indeplineste cerintele dvs, avand aria: + aria -> daca aria e mai mare ca 39
+            //Exercitiu 4
+            //todo - Creeaza un Array de Integers
+            //todo - Adauga in Array 10 integers {1, 18, 6, 9, 4, 20, 110, 3250, 10, 8}
+            //todo - Printeaza numerele atata timp cat ele sunt mai mari sau egal decat 9
+            //todo - Foloseste instructiunea de salt do while
+            //Exercitiu 5
+            //todo - Creeaza un Array de Integers cu urmatoarele {10, 18, 15, 17, 19, 21, 19, 32, 54, 17}
+            //todo - Numara cate numere distincte se afla intre (>=) 10 si 20 (<=)
+            //todo - Daca sunt 6: Printeaza "Suceava"
+            //todo - Daca sunt 5: Printeaza "Botosani"
+            //todo - Daca sunt 4: Printeaza "Vaslui"
+            //todo - Foloseste instructiunea de salt switch case
+
+
+
+    }
+}
---
+++ src/main/java/Sesion1/MainPresentation.java
@@ -0,0 +1,7 @@
+package Sesion1;
+
+public class MainPresentation {
+    public static void main(String[] args) {
+
+    }
+}
