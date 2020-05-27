362fac3e62888d1a893cedaf6f70ccae92edb754
Adrian Hurjui <adrian.hurjui@endava.com>
2020-05-27 10:00:45 +0300

    updated homework

--- src/main/java/Sesion1/MainHomework.java
+++ src/main/java/Sesion1/MainHomework.java
@@ -7,7 +7,7 @@ public class MainHomework {
             //Exercitiu 1
             //todo - Creeaza un vector de String-uri;
             //todo - Adauga in vector 6 prenume de persoane [Andrei, Alex, Ioana, Adrian, Liviu, Cosmin];
-            //todo - Itereaza vectorul si printeaza toate numele care contin litera "a" in prenume;
+            //todo - Itereaza vectorul si printeaza toate prenumele care contin litera "a" in prenume;
             //Exercitiu 2
             //todo - Creeaza un vector de double
             //todo - Adauga in vector 10 integer-uri [10, 11, 13, 15, 18, 17, 20, 25, 31, 49]
@@ -20,18 +20,15 @@ public class MainHomework {
             //todo - Printeaza raspunsul: Cercul nu indeplineste cerintele dvs, avand aria: + aria -> daca aria e mai mare ca 39
             //Exercitiu 4
             //todo - Creeaza un Array de Integers
-            //todo - Adauga in Array 10 integers {1, 18, 6, 9, 4, 20, 110, 3250, 10, 8}
+            //todo - Adauga in Array 10 integers {9, 1, 6, 20, 9, 20, 110, 3250, 10, 8}
             //todo - Printeaza numerele atata timp cat ele sunt mai mari sau egal decat 9
-            //todo - Foloseste instructiunea de salt do while
+            //todo - Foloseste instructiunea de salt while
             //Exercitiu 5
             //todo - Creeaza un Array de Integers cu urmatoarele {10, 18, 15, 17, 19, 21, 19, 32, 54, 17}
             //todo - Numara cate numere distincte se afla intre (>=) 10 si 20 (<=)
             //todo - Daca sunt 6: Printeaza "Suceava"
             //todo - Daca sunt 5: Printeaza "Botosani"
-            //todo - Daca sunt 4: Printeaza "Vaslui"
-            //todo - Foloseste instructiunea de salt switch case
-
-
-
+            //todo - Daca sunt 7: Printeaza "Vaslui"
+            //todo - Foloseste instructiunea de switch case
     }
 }
