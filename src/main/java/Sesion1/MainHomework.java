a2a33050f6b06cea3ce078dd4ff187b42aaffc08
Adrian Hurjui <adrian.hurjui@endava.com>
2020-06-10 09:53:47 +0300

    changes added on homework

--- src/main/java/Sesion1/MainHomework.java
+++ src/main/java/Sesion1/MainHomework.java
@@ -5,19 +5,18 @@ public class MainHomework {
 
         // HOMEWORK//
             //Exercitiu 1
-            //todo - Creeaza un vector de String-uri;
-            //todo - Adauga in vector 6 prenume de persoane [Andrei, Alex, Ioana, Adrian, Liviu, Cosmin];
-            //todo - Itereaza vectorul si printeaza toate prenumele care contin litera "a" in prenume;
+            //todo - Creeaza o lista de String-uri;
+            //todo - Adauga in lista 6 prenume de persoane [Andrei, Alex, Ioana, Adrian, Liviu, Cosmin];
+            //todo - Itereaza lista si printeaza toate prenumele care contin litera "a" in prenume;
             //Exercitiu 2
             //todo - Creeaza un vector de double
-            //todo - Adauga in vector 10 integer-uri [10, 11, 13, 15, 18, 17, 20, 25, 31, 49]
-            //todo - Adauga in vector 4 double-uri [10.5, 16.8, 19.5, 20.00]
-            //todo - Itereaza vectorul si printeaza toate numerele >= 10 si <=20 care nu sunt de tip double
+            //todo - Adauga in vector 4 double-uri [10.5, 16.8, 21.5, 24.5, 19.5, 20.00]
+            //todo - Itereaza vectorul si printeaza toate numerele >= 10 si <=20 rotunjite ca un integer (e.g. 10.5 -> 10, 16.8 -> 16)
             //Exercitiu 3
             //todo - Avand urmatorul detaliu: Cerc cu un diametru de 7
             //todo - Sa se afle aria discului de cerc. Dar nu prin formula (pi * raza la patrat). Valoarea lui pi = 3.14;
-            //todo - Printeaza raspunsul: Cercul indeplineste cerintele dvs, avand aria: + aria -> daca aria e mai mare ca 38
-            //todo - Printeaza raspunsul: Cercul nu indeplineste cerintele dvs, avand aria: + aria -> daca aria e mai mare ca 39
+            //todo - Printeaza raspunsul: Cercul indeplineste cerintele dvs, avand aria: + aria -> daca aria e mai mare ca 38.00 si mai mica ca 39.00
+            //todo - Printeaza raspunsul: Cercul nu indeplineste cerintele dvs, avand aria: + aria -> daca aria e mai mare ca 39.00
             //Exercitiu 4
             //todo - Creeaza un Array de Integers
             //todo - Adauga in Array 10 integers {9, 1, 6, 20, 9, 20, 110, 3250, 10, 8}
--- src/main/java/Sesion1/MainPresentation.java
+++ src/main/java/Sesion1/MainPresentation.java
@@ -1,7 +1,70 @@
 package Sesion1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainPresentation {
     public static void main(String[] args) {
 
+        String string = "Adrian";
+
+        String[] vectors1 = new String[6];
+
+        vectors1[1] = "10";
+        vectors1[0] = "11";
+        vectors1[2] = "12";
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(11);
+        list.add(12);
+        list.add(14);
+        list.add(11);
+        list.add(15);
+        list.add(18);
+
+        int i = 0;
+        while (i < list.size() && list.get(i) < 14) {
+            System.out.println(list.get(i));
+            i++;
+        }
+
+        Double[] vectors = new Double[6];
+
+        vectors[1] = 10.00;
+        vectors[0] = 11.00;
+        vectors[2] = 12.00;
+
+        int count = 0;
+
+
+//        do {
+//            System.out.println(list.get(i));
+//            i++;
+//        } while (i < list.size() && list.get(i) < 14)
+
+        ;
+//        for (int index = 0; index < vectors.length; i++) {
+//            if (vectors[i] > 12) {
+//                count++;
+//            }
+//        }
+
+//        for (int vector : vectors
+//             ) {
+//            System.out.println(vector);
+//        }
+
+//        switch (count) {
+//            case 1:
+//                System.out.println("prima" + count);
+//            case 2:
+//                System.out.println("doua " + count);
+//                break;
+//            case 3:
+//                System.out.println("treia");
+//                break;
+//            default:
+//                System.out.println("nada");
+//        }
     }
 }
